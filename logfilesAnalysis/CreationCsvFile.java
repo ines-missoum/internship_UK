f059059c70aa05351a6c5c1b54cc7a8d7d5157f9
ines-missoum <32544041+ines-missoum@users.noreply.github.com>
2018-07-02 11:59:39 +0100

    csv file for an entire logfile + number of example
    The csv file is now created for an entire logfile with no repetition of pattern. The number of examples for each pattern is counted.

--- logfilesAnalysis/CreationCsvFile.java
+++ logfilesAnalysis/CreationCsvFile.java
@@ -11,6 +11,7 @@ public class CreationCsvFile {
 	public static void main(String[] args) throws IOException {
 
 		ArrayList<String> regExps = new ArrayList<String>();
+		ArrayList<Type> types = new ArrayList<Type>();
 
 		/* collect names of regEx */
 		String regExNames = ";";
@@ -35,43 +36,64 @@ public class CreationCsvFile {
 
 		} finally {
 			br.close();
-			regExNames+="\n";
+			regExNames += "\n";
 		}
 
 		/* creation of the csv file */
 
 		FileWriter file = new FileWriter("lattice.csv");
-		BufferedReader br2 = new BufferedReader(new FileReader("syslog.txt"));
+		BufferedReader br2 = new BufferedReader(new FileReader("syslog.txt"));/* logfile */
 		String lineCSV;
 		String line_logfile;
-		try {
-			file.append(regExNames);
+		String pattern="";
 
-			line_logfile = br2.readLine(); /*for the first line of the logfile*/
-			
-			for(int j=0; j<=1; j++) {
-				System.out.println(line_logfile);
+		try {
+			file.append(regExNames);/* creation of the first line of the csv file (names of regExp) */
+
+			/* for all the lines of the logfiles */
+
+			line_logfile = br2.readLine();
+			while (line_logfile != null) {
+				line_logfile = br2.readLine(); /* for the first line of the logfile */
+				
+//				 /******** just to choose the line ==> to erase *************/
+//				 for (int j = 0; j <= 1; j++) {
+				// System.out.println(line_logfile);
+				 // line_logfile = br2.readLine();
+				// }
+				// System.out.println(line_logfile);
+				// /*************************************************************/
+
+				String[] parts = line_logfile.split("[, ]");
+				Type type;
+				for (int i = 0; i < parts.length; i++) {
+					/* For each part of the logfile line we build a csv line : */
+					pattern="";
+					//System.out.println(parts[i]);
+					for (String s : regExps) {
+
+						if (parts[i].matches(s)) {
+							pattern += " 1;";
+						} else {
+							pattern += " 0;";
+						}
+					}
+					
+					lineCSV = parts[i] + ";"+pattern;
+
+					/* a line per type */
+					type = Type.getType(pattern, types);
+
+					if (type.getNumberOfExamples() == 0) {/* new type */
+						types.add(type);
+						lineCSV += " \n";
+						file.append(lineCSV);
+						System.out.println(parts[i]);
+					}
+					type.addExample();
+				}
 				line_logfile = br2.readLine();
 			}
-			System.out.println(line_logfile);
-			String[] parts = line_logfile.split("[, ]");//"[:, ]"
-					
-			for( int i=0; i<parts.length;i++) {
-				/*we build the line : */
-				lineCSV=parts[i]+";";
-				System.out.println(parts[i]);
-				for(String s : regExps) {
-					
-					if (parts[i].matches(s)) {
-						lineCSV+=" 1;";
-					}else {
-						lineCSV+=" 0;";
-					}
-				}
-				lineCSV+=" \n";
-				file.append(lineCSV);
-			}
-	
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
